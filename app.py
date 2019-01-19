841b037055bd1bc8d726416924970a64251dacbc
mohan vashist <mohanvashist76@hotmail.com>
2019-01-19 10:38:52 -0500

    added new car stuff

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ client = smartcar.AuthClient(
     client_id='8af861e3-570a-45f5-b321-ef755778ff42',
     client_secret='5347869a-217b-4775-ae3e-c5ed66380c82',
     redirect_uri='http://localhost:5000/smartcar/exchange',
-    scope=['read_vehicle_info'],
-    test_mode=True
+    scope=['read_vehicle_info', 'control_security', 'read_odometer'],
+    test_mode=False
 )
 
 
@@ -49,7 +49,7 @@ def borrow():
     return render_template("borrow.html")
 
 
-@app.route('/smarcar/login', methods=['GET'])
+@app.route('/smartcar/login', methods=['GET'])
 def smart_login():
     auth_url = client.get_auth_url()
     return redirect(auth_url)
@@ -67,7 +67,7 @@ def smart_exchange():
     return '', 200
 
 
-@app.route('/smartcar/vehicle', methods=['GET'])
+@app.route('/smartcar/vehicles', methods=['GET'])
 def smart_vehicle():
     # access our global variable to retrieve our access tokens
     global access
@@ -83,6 +83,35 @@ def smart_vehicle():
 
     return jsonify(info)
 
+@app.route('/smartcar/unlock', methods=['GET'])
+def smart_unlock():
+    global access
+    vehicle_ids = smartcar.get_vehicle_ids(
+        access['access_token'])['vehicles']
+    vehicle = smartcar.Vehicle(vehicle_ids[0], access['access_token'])
+    print(vehicle)
+    vehicle.unlock()
+    return ""
+@app.route('/smartcar/lock', methods=['GET'])
+def smart_lock():
+    global access
+    vehicle_ids = smartcar.get_vehicle_ids(
+        access['access_token'])['vehicles']
+    vehicle = smartcar.Vehicle(vehicle_ids[0], access['access_token'])
+    print(vehicle)
+    vehicle.lock()
+    return ""
+
+@app.route('/smartcar/odom', methods=['GET'])
+def read_odom():
+    global access
+    vehicle_ids = smartcar.get_vehicle_ids(
+        access['access_token'])['vehicles']
+    vehicle = smartcar.Vehicle(vehicle_ids[0], access['access_token'])
+    response = vehicle.odometer()
+    print(response)
+    print(response["data"]["distance"])
+    return ""
 
 if __name__ == '__main__':
     app.run(port=5000)
