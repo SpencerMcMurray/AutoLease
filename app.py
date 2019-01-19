7b1e184a1e768aecf4cb3c71170e8a5d599dc36b
mcmurr32 <spencermcmurray@yahoo.ca>
2019-01-19 03:28:19 -0500

    Added pages

--- app.py
+++ app.py
@@ -4,8 +4,27 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def home():
+    """ The Home Page """
     return render_template("home.html")
+
+
+@app.route('/settings')
+def settings():
+    """ The Settings Page """
+    return render_template("settings.html")
+
+
+@app.route('/login')
+def login():
+    """ The Log In Page """
+    return render_template("login.html")
+
+
+@app.route('/signup')
+def signup():
+    """ The Sign Up Page """
+    return render_template("signup.html")
 
 
 if __name__ == '__main__':
