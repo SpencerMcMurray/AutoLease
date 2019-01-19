057cac2e190588a3fc7ca85dab2e46588f18ef31
mcmurr32 <spencermcmurray@yahoo.ca>
2019-01-19 05:14:14 -0500

    Updated sign up and login, and started to add database support;

---
+++ functions/database.py
@@ -0,0 +1,14 @@
+import pymysql
+
+
+class Database:
+    def __init__(self):
+        host = "den1.mysql5.gear.host"
+        port = 3306
+        user = "autolease"
+        password = "Em56bP59!21_"
+        db = "autolease"
+
+        self.con = pymysql.connect(host=host, port=port, user=user, password=password, db=db,
+                                   cursorclass=pymysql.cursors.DictCursor, autocommit=True)
+        self.cur = self.con.cursor()
