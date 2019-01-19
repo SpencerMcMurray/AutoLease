4e3851fcdefec38ea530b6d8d2b2f20c26b8299c
mohan vashist <mohanvashist76@hotmail.com>
2019-01-19 08:38:55 -0500

    merged

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template
 import functions.basic_functions as bf
 import datetime
+import smartcar
+from flask import Flask, redirect, request, jsonify, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+# global variable to save our access_token
+access = None
+
+client = smartcar.AuthClient(
+    client_id='8af861e3-570a-45f5-b321-ef755778ff42',
+    client_secret='5347869a-217b-4775-ae3e-c5ed66380c82',
+    redirect_uri='http://localhost:5000/smartcar/exchange',
+    scope=['read_vehicle_info'],
+    test_mode=True
+)
 
 
 @app.route('/')
@@ -35,5 +49,41 @@ def borrow():
     return render_template("borrow.html")
 
 
+@app.route('/smarcar/login', methods=['GET'])
+def smart_login():
+    auth_url = client.get_auth_url()
+    return redirect(auth_url)
+
+
+@app.route('/smartcar/exchange', methods=['GET'])
+def smart_exchange():
+    code = request.args.get('code')
+
+    # access our global variable and store our access tokens
+    global access
+    # in a production app you'll want to store this in some kind of
+    # persistent storage
+    access = client.exchange_code(code)
+    return '', 200
+
+
+@app.route('/smartcar/vehicle', methods=['GET'])
+def smart_vehicle():
+    # access our global variable to retrieve our access tokens
+    global access
+    # the list of vehicle ids
+    vehicle_ids = smartcar.get_vehicle_ids(
+        access['access_token'])['vehicles']
+
+    # instantiate the first vehicle in the vehicle id list
+    vehicle = smartcar.Vehicle(vehicle_ids[0], access['access_token'])
+
+    info = vehicle.info()
+    print(info)
+
+    return jsonify(info)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000)
+
