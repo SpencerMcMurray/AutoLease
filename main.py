7a329058ad35063d63d2c13c0599f8d55e6c5907
Soham-pathak <soham.pathak@mail.utoronto.ca>
2019-01-20 07:38:59 -0500

    Fixed bug in while-loop structure.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import base64
-import random #for salt
+import random  # for salt
 import hashlib
 import time
 import authentication as authentication
@@ -7,9 +7,9 @@ import moneyRequests as moneyRequests
 
 url2 = 'https://gateway-web.beta.interac.ca/publicapi/api/v2/'
 
-def main(email_id,amount):
-    
-    secretKey  = 'k5PG9-GT0x016M-rKZPqpCPQYj2HFlLXTU8kJpQl5L8'
+
+def main(email_id, amount):
+    secretKey = 'k5PG9-GT0x016M-rKZPqpCPQYj2HFlLXTU8kJpQl5L8'
     salt = createSalt()
     keyAndSalt = salt + ':' + secretKey
     thirdPartyAccessid = 'CA1TAuUG9Ned35wF'
@@ -29,45 +29,55 @@ def main(email_id,amount):
 
     # contacts.addContact(access_token, thirdPartyAccessid, requestId, deviceID, apiRegistrationId, email, name)
     # contacts.getContacts(access_token, thirdPartyAccessid, requestId, deviceID, apiRegistrationId)
-    #contacts.putContact(access_token, thirdPartyAccessid, requestId, deviceID, apiRegistrationId, email, name, contactID)
-    #contacts.deleteContact(access_token, thirdPartyAccessid, requestId, deviceID, apiRegistrationId, contactID)
-    #contacts.getContactThroughContactID(access_token, thirdPartyAccessid, requestId, deviceID, apiRegistrationId, contactID)
+    # contacts.putContact(access_token, thirdPartyAccessid, requestId, deviceID, apiRegistrationId, email, name, contactID)
+    # contacts.deleteContact(access_token, thirdPartyAccessid, requestId, deviceID, apiRegistrationId, contactID)
+    # contacts.getContactThroughContactID(access_token, thirdPartyAccessid, requestId, deviceID, apiRegistrationId, contactID)
 
-    
-    #money
+    # money
     # moneyRequests.sendMoneyRequest(access_token, thirdPartyAccessid, requestId, deviceID, apiRegistrationId, fromDate, toDate)
-    payload = moneyRequests.getMoneyRequest(access_token, thirdPartyAccessid, requestId, deviceID, apiRegistrationId, fromDate, toDate,'CA1MRjpTs4wQ')
-    link = moneyRequests.sendMoneyRequestOneTimeContact(access_token,
-                                                        thirdPartyAccessid,
-                                                        requestId, deviceID,
-                                                        apiRegistrationId,
-                                                        fromDate, toDate,
-                                                        amount)
+    payload = moneyRequests.getMoneyRequest(access_token, thirdPartyAccessid,
+                                            requestId, deviceID,
+                                            apiRegistrationId, fromDate,
+                                            toDate, 'CA1MRke7Zh83')
     status = moneyRequests.get_status_from_dict(payload)
-    while (status not in "78"):
+    while (not (status is ("7") or status is ("8"))):
         print(status)
+        payload = moneyRequests.getMoneyRequest(access_token,
+                                                thirdPartyAccessid, requestId,
+                                                deviceID, apiRegistrationId,
+                                                fromDate, toDate, slug)
         link = moneyRequests.sendMoneyRequestOneTimeContact(access_token,
-                                                        thirdPartyAccessid,
-                                                        requestId, deviceID,
-                                                        apiRegistrationId,
-                                                        fromDate, toDate,
-                                                        amount)
+                                                            thirdPartyAccessid,
+                                                            requestId,
+                                                            deviceID,
+                                                            apiRegistrationId,
+                                                            fromDate, toDate,
+                                                            amount)
+
         slug = link.split("/")[-1]
-        payload = moneyRequests.getMoneyRequest(access_token, thirdPartyAccessid, requestId, deviceID, apiRegistrationId, fromDate, toDate,'CA1MRS2QrhGr')
-        time.sleep(15)
+        time.sleep(20)
+        status = moneyRequests.get_status_from_dict(payload)
+        print(status)
+    # print(ref)
+    # print(moneyRequests.get_status_from_dict(ref))
+    # while(moneyRequests.get_status_from_dict(ref)!='8' or moneyRequests.get_status_from_dict(ref)!='7'):
+    #     ref = link.split("/")[-1]
+
 
 def encodeSecretKey(keyAndSalt):
     h = hashlib.sha256()
     h.update(keyAndSalt.encode())
-    return base64.b64encode(h.digest()).decode("utf-8") 
+    return base64.b64encode(h.digest()).decode("utf-8")
+
 
 def createSalt():
     ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    chars=[]
+    chars = []
     for i in range(16):
         chars.append(random.choice(ALPHABET))
-    
+
     return "".join(chars)
 
+
 if __name__ == '__main__':
-    main("sohampathak991@icloud.com","100")
+    main("sohampathak991@icloud.com", "100")
